47f141a27d8c72736afcb5c993f7ed540593e654
saintanio <75741869+saintanio@users.noreply.github.com>
2025-09-16 17:35:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,41 @@
+const CACHE_NAME = "mon-cache-v1";
+const ASSETS = [
+  " /newstore/", // à adapter selon ton hébergement (sur GitHub Pages, mets '/newstore/')
+  " /newstore/index.html",
+  " /newstore/js/app.js",
+  " /newstore/main.css",
+   "/newstore/manifest.json", 
+  " /newstore/images/icon-192.png",
+  " /newstore/images/icon-512.png",
+  // ajoute ici tous les autres fichiers nécessaires pour ton app offline
+];
 
 self.addEventListener("install", event => {
   console.log("SW installé");
-  self.skipWaiting(); // ⚠ Force l'activation immédiate
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   console.log("SW activé");
-  clients.claim(); // Prend le contrôle sans rechargement manuel
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
 });
+
+
